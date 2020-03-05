436ce90f5c2118f03f38f59df5a5a5b80382c330
Tetiana <t.pasichnyk@ukr.net>
2020-03-06 00:42:37 +0200

    sum arr ^2

--- Homework9.js
+++ Homework9.js
@@ -4,4 +4,11 @@
 // let arr=[1,2,3];
 // arr.unshift(4,5,6);
 // alert(arr);
-let arr=[1,2,3,4,5,6,7,8,9,10];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,10];
+var sum=0;
+
+for (let i = 0; i < arr.length; i+=1) {
+    sum=sum+arr[i]*arr[i];
+
+}
+alert( sum );
