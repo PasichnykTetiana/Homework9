80938f14ba67e626bdd91e168577a646bc5f676f
Tetiana <t.pasichnyk@ukr.net>
2020-03-09 02:10:13 +0200

    random

--- Homework9.js
+++ Homework9.js
@@ -4,11 +4,20 @@
 // let arr=[1,2,3];
 // arr.unshift(4,5,6);
 // alert(arr);
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,10];
-var sum=0;
-
-for (let i = 0; i < arr.length; i+=1) {
-    sum=sum+arr[i]*arr[i];
-
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,10];
+// // var sum=0;
+// //
+// // for (let i = 0; i < arr.length; i+=1) {
+// //     sum=sum+arr[i]*arr[i];
+// //
+// // }
+// // alert( sum );
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-alert( sum );
+let arr=[];
+arr.length=10;
+for (let i=0; i<arr.length; i+=1){
+    arr[i]=getRandomInRange(1,10);
+}
+alert(arr);
